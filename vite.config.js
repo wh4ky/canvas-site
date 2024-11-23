7cba41596c5032679296c79270d2bd938be83dd8
wh4ky <damian.whacky@proton.me>
2024-11-23 23:04:36 +0100

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/your-repository-name/", // Replace with your actual repository name
+  build: {
+    outDir: "dist", // This is the default setting
+  },
+});
