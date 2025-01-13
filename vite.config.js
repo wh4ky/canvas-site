3f55b2deb3a1265885764a8987ed5774021d613e
Damian Czapnik <damian.whacky@proton.me>
2025-01-13 22:17:56 +0100

    updated to es2022
    TODO: Build zig to public/ so vite can see it.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/canvas-site/",
   build: {
-    target: "es2017",
+    target: "es2022",
     outDir: "dist",
   },
 });
